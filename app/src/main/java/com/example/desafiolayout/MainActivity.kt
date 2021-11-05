c7bd61cc71c2f84a480ec1dc53337693b354a969
Fernando Zarate <ext.fernando.vieira@dafiti.com.br>
2021-11-05 17:41:33 -0300

    Removed magic number

--- app/src/main/java/com/example/desafiolayout/MainActivity.kt
+++ app/src/main/java/com/example/desafiolayout/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.desafiolayout
 
-import android.graphics.Rect
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.desafiolayout.adapter.ItemAdapter
 import com.example.desafiolayout.data.Datasource
-import androidx.recyclerview.widget.GridLayoutManager
+import com.example.desafiolayout.model.QuickAccess
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,20 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myDataset = Datasource().loadButtons()
-        val recyclerView = findViewById<RecyclerView>(R.id.quickAccess)
-        recyclerView.layoutManager = GridLayoutManager(this, 3)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.addItemDecoration(object: RecyclerView.ItemDecoration() {
-            override fun getItemOffsets(
-                outRect: Rect,
-                view: View,
-                parent: RecyclerView,
-                state: RecyclerView.State
-            ) {
-                super.getItemOffsets(outRect, view, parent, state)
-            }
-        })
-        recyclerView.adapter = ItemAdapter(this, myDataset)
+        setupRecyclerView(myDataset)
+    }
 
+    private fun setupRecyclerView(myDataset: List<QuickAccess>) {
+        val recyclerView = findViewById<RecyclerView>(R.id.quickAccess)
+        recyclerView.layoutManager = GridLayoutManager(this, QUICK_ACCESS_COLUMNS)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+    }
+
+    private companion object {
+        const val QUICK_ACCESS_COLUMNS = 3
     }
 }
