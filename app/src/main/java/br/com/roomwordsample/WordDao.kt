76ec428864c0f695689a12b55ab4e78c44d9f161
Paulo César Messias dos Santos <paulo.messias@dextra-sw.com>
2022-02-14 15:36:05 -0300

    Android Room with a View initial commit

---
+++ app/src/main/java/br/com/roomwordsample/WordDao.kt
@@ -0,0 +1,22 @@
+package br.com.roomwordsample
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+
+    // The flow always holds/caches latest version of data. Notifies its observers when the
+    // data has changed.
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): Flow<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
